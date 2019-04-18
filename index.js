fda07441791b547424e6d7d0b21337c013127518
Jadson Luan <jadsonluan1@gmail.com>
2019-04-18 18:59:40 -0300

    Implementa e renderiza pagina de autenticação

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const axios = require('axios');
-var data;
-axios({
-  url: 'https://github.com/login/oauth/authorize',
-  method: 'get',
-  data: {
-    client_id: 29259
-  }
-}).then(r => console.log(r));
+var axios = require('axios');
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  axios({
+    url: 'https://github.com/login/oauth/authorize',
+    method: 'get',
+    data: {
+      client_id: 29259
+    }
+  }).then(response => {
+    res.send(response.data);
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
